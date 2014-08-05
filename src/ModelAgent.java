edcbac98aa40cb01968fc989d733d1f049c169dc
Bryan Head <bryan.head@gmail.com>
2014-08-05 11:07:53 -0500

    wip Update ModelAgent to new Agent interface

--- src/ModelAgent.java
+++ src/ModelAgent.java
@@ -1,65 +1,64 @@
-import org.nlogo.api.Argument;
+import org.nlogo.agent.*;
 import org.nlogo.api.ExtensionException;
 import org.nlogo.api.LogoException;
+import org.nlogo.nvm.ReporterTask;
 import org.nlogo.nvm.CommandTask;
-import org.nlogo.nvm.ExtensionContext;
+import org.nlogo.nvm.Context;
+import org.nlogo.api.Task;
 
 
 public class ModelAgent implements Agent {
-	Model theModel;
+	Model model;
 	
-	public ModelAgent(Model theModel){
-		this.theModel = theModel;
+	public ModelAgent(Model model){
+		this.model = model;
 	}
 	
 
-	public void ask(Argument args[], org.nlogo.api.Context context) throws ExtensionException, LogoException{
-		boolean killModel = false;
-
-		Object rawCommand = args[1].get();
-		int n = args.length - 2;
-		Object[] actuals = new Object[n];
-		for (int i = 0; i < n; i++) {
-			actuals[i] = args[i+2].get();
-		}
-			CommandTask task;
-			if (rawCommand instanceof CommandTask) {
-				task = (CommandTask) rawCommand;
-			} else {
-				String command = rawCommand.toString();
-				if (actuals.length > 0) {
-
-					task = (CommandTask) theModel.report("task [ " + command + " ]");
-				} else {
-					// No arguments, don't bother making a task and such
-					theModel.command(command);
-					return;
-				}
-			}
-			theModel.command(((ExtensionContext) context).nvmContext(), task, actuals);
-
-	}
 	@Override
-	public Object of(String s) throws ExtensionException {
-		Object reportedValue = theModel.report(s);
-		// We need to deal with different return types
-		// If the model reports an agent, we need to wrap it in the TurtleAgent class
-		if (reportedValue instanceof org.nlogo.agent.Agent){
-			org.nlogo.api.Agent theAgent = (org.nlogo.api.Agent)reportedValue;
-			return new TurtleAgent(this, theAgent);
-		}
-		// If the model reports an agentset, we need to wrap it in the LevelSpaceAgentSet class
-		else if (reportedValue instanceof org.nlogo.api.AgentSet){
-			org.nlogo.api.AgentSet reportedAgentSet = (org.nlogo.api.AgentSet)reportedValue;
-			AgentSetAgent returnAgentSet = new AgentSetAgent();
-			for (org.nlogo.api.Agent agent : reportedAgentSet.agents()){
-				TurtleAgent anAgent = new TurtleAgent(this, agent);
-				returnAgentSet.add(anAgent);
-			}
-			return returnAgentSet;
-		}
-		else {
-			return reportedValue;
+	public void ask(Context parentContext, String command, Object[] args) throws ExtensionException, LogoException{
+		ask(parentContext, compile(CommandTask.class, command), args);
+	}
+
+	@Override
+	public void ask(Context parentContext, CommandTask command, Object[] args) throws ExtensionException, LogoException {
+		ask(parentContext, model.workspace().world().observer(), command, args);
+	}
+
+	public void ask(Context parentContext, org.nlogo.agent.Agent agent, String command, Object[] args) throws ExtensionException, LogoException {
+		ask(parentContext, agent, compile(CommandTask.class, command), args);
+	}
+
+	public void ask(Context parentContext, org.nlogo.agent.Agent agent, CommandTask command, Object[] args) throws ExtensionException, LogoException {
+		Context context = new Context(parentContext, agent);
+		context.agent = agent;
+		model.command(context, command, args);
+	}
+
+	@Override
+	public Object of(Context parentContext, String reporter, Object[] args) throws ExtensionException, LogoException {
+		return of(parentContext, compile(ReporterTask.class, reporter), args);
+	}
+
+	@Override
+	public Object of(Context parentContext, ReporterTask reporter, Object[] args) throws ExtensionException, LogoException {
+		return of(parentContext, model.workspace().world().observer(), reporter, args);
+	}
+
+	public Object of(Context parentContext, org.nlogo.agent.Agent agent, String reporter, Object[] args) throws ExtensionException, LogoException {
+		return of(parentContext, agent, compile(ReporterTask.class, reporter), args);
+	}
+
+	public Object of(Context parentContext, org.nlogo.agent.Agent agent, ReporterTask reporter, Object[] args) throws ExtensionException, LogoException {
+		return model.report(new Context(parentContext, agent), reporter, args);
+	}
+
+	public <T extends Task> T compile(Class<T> taskType, String code) throws ExtensionException {
+		Object task = model.report("task [ " + code + " ]");
+		if (taskType.isInstance(task)) {
+			return taskType.cast(task);
+		} else {
+			throw new ExtensionException(String.format("Needed a %s but `%s` compiles to a %s.", taskType, code, task.getClass()));
 		}
 	}
 }
