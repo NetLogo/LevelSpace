19a98a78837747104a2adc4d94a75d8388771d57
Bryan Head <bryan.head@gmail.com>
2014-08-05 11:07:23 -0500

    wip Update Agent interface

--- src/Agent.java
+++ src/Agent.java
@@ -1,10 +1,13 @@
-import org.nlogo.api.Argument;
 import org.nlogo.api.ExtensionException;
 import org.nlogo.api.LogoException;
+import org.nlogo.nvm.Context;
+import org.nlogo.nvm.CommandTask;
+import org.nlogo.nvm.ReporterTask;
 
 
 public interface Agent {
-	public void ask(Argument[] args, org.nlogo.api.Context context) throws ExtensionException, LogoException;
-	public Object of(String s) throws ExtensionException;
-
+	public void ask(Context parentContext, String command, Object[] args) throws ExtensionException, LogoException;
+	public void ask(Context parentContext, CommandTask command, Object[] args) throws ExtensionException, LogoException;
+	public Object of(Context parentContext, String reporter, Object[] args) throws ExtensionException, LogoException;
+	public Object of(Context parentContext, ReporterTask reporter, Object[] args) throws ExtensionException, LogoException;
 }
