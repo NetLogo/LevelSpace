6fa2f28817d2a20931441916455b9e30fd012d85
Bryan Head <bryan.head@gmail.com>
2014-08-05 11:47:56 -0500

    Implement new Agent interface in TPLAgent

--- src/TPLAgent.java
+++ src/TPLAgent.java
@@ -6,32 +6,36 @@ import org.nlogo.nvm.CommandTask;
 import org.nlogo.nvm.ReporterTask;
 
 
+/**
+ * Agent wrapper for turtle, patches, and links.
+ */
 public class TPLAgent implements Agent {
 	ModelAgent parentModel;
-	org.nlogo.api.Agent agent;
+	org.nlogo.agent.Agent agent;
 
-	public TPLAgent(ModelAgent parentModel, org.nlogo.api.Agent agent){
+	public TPLAgent(ModelAgent parentModel, org.nlogo.agent.Agent agent){
 		this.parentModel = parentModel;
 		this.agent = agent;
 	}
 	
 	@Override
 	public void ask(org.nlogo.nvm.Context parentContext, String command, Object[] args) throws ExtensionException, LogoException {
-
+		parentModel.ask(parentContext, agent, command, args);
 	}
 
 	@Override
 	public void ask(org.nlogo.nvm.Context parentContext, CommandTask command, Object[] args) throws ExtensionException, LogoException {
+		parentModel.ask(parentContext, agent, command, args);
 
 	}
 
 	@Override
 	public Object of(org.nlogo.nvm.Context parentContext, String reporter, Object[] args) throws ExtensionException, LogoException {
-		return null;
+		return parentModel.of(parentContext, agent, reporter, args);
 	}
 
 	@Override
 	public Object of(org.nlogo.nvm.Context parentContext, ReporterTask reporter, Object[] args) throws ExtensionException, LogoException {
-		return null;
+		return parentModel.of(parentContext, agent, reporter, args);
 	}
 }
