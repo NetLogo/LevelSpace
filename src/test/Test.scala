f123fae4b4dfda41edb35cd998e2ee6dbdaaa3fb
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2021-05-25 14:23:59 -0500

    Tests: Make sure to run headlessly

--- src/test/Test.scala
+++ src/test/Test.scala
@@ -2,4 +2,6 @@ package org.nlogo.ls
 
 import org.nlogo.headless.TestLanguage
 
-class Tests extends TestLanguage(Seq(new java.io.File("tests.txt").getCanonicalFile))
+class Tests extends TestLanguage(Seq(new java.io.File("tests.txt").getCanonicalFile)) {
+  System.setProperty("org.nlogo.preferHeadless", "true")
+}
